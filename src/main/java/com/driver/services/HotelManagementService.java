95e1b3fb16c496a72c71ffe2551d8cc18b3456e0
Mohammad Maviya Waseem Shaikh <maviya9830@gmail.com>
2023-09-04 15:11:54 +0530

    submit

--- src/main/java/com/driver/services/HotelManagementService.java
+++ src/main/java/com/driver/services/HotelManagementService.java
@@ -13,8 +13,8 @@ import java.util.List;
 @Service
 public class HotelManagementService {
 
-    @Autowired
-    HotelManagementRepository hotelManagementRepository;
+
+    HotelManagementRepository hotelManagementRepository = new HotelManagementRepository();
 
 
     public String addHotel(Hotel hotel) {
